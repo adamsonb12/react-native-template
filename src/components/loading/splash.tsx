1daa288814176a89af896779867dc541b0c12ecc
adamsonb12 <adamsonb12@gmail.com>
2022-09-07 14:58:03 -0700

    updated so web works locally, though lottie doesn't support it so it's wrapped with a platform check

--- src/components/loading/splash.tsx
+++ src/components/loading/splash.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components/native";
 import LottieView from "lottie-react-native";
 
 import { ScreenView } from "../@common/layout";
+import { Platform } from "react-native";
+import { TitleLarge } from "../@common/typography/title";
 
 const StyledScreenContainer = styled(ScreenView)`
   display: flex;
@@ -13,6 +15,15 @@ const StyledScreenContainer = styled(ScreenView)`
 
 export const Splash = () => {
   const animation = useRef(null);
+
+  // Lottie does not work in web in RN app
+  if (Platform.OS === "web") {
+    return (
+      <StyledScreenContainer>
+        <TitleLarge>Loading...</TitleLarge>
+      </StyledScreenContainer>
+    );
+  }
 
   return (
     <StyledScreenContainer>
