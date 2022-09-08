12561449ded858b318ba7b90405110b89767979e
Jason Hamm <jasonhamm.me@gmail.com>
2022-09-07 19:23:09 -0600

    Add GraphQL client

---
+++ config/apollo.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useEffect, useState } from "react";
+import {
+  ApolloProvider as Provider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+import { GRAPHQL_ENDPOINT, ENABLE_GRAPHQL } from "@env";
+
+export const createClient = () =>
+  new ApolloClient({
+    uri: GRAPHQL_ENDPOINT,
+    cache: new InMemoryCache(),
+  });
+
+export const ApolloProvider = ({ children }: { children: ReactNode }) => {
+  const [client] = useState(createClient());
+
+  return ENABLE_GRAPHQL ? (
+    <Provider client={client}>{children}</Provider>
+  ) : (
+    <>{children}</>
+  );
+};
